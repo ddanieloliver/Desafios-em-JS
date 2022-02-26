a8f9002adb0a4b01dc64559091172065aecb1249
ddanieloliver <danielvitoroliveira456@gmail.com>
2022-02-26 18:18:15 -0300

    Enunciado confuso

---
+++ Exercício13.js
@@ -0,0 +1,18 @@
+/*Crie uma função que receba um array de elementos e retorne um array somente com os números presentes no
+array recebido como parâmetro.
+Exemplos:
+filtrarNumeros(["Javascript", 1, "3", "Web", 20]) // retornará [1, 20]
+filtrarNumeros(["a", "c"]) // retornará []*/
+
+function filtrarNumeros(array){
+   /* const resultado = []
+    for(let item of array){
+        if(typeof item === "number"){
+            resultado.push(item)
+        }
+    } return resultado*/
+    return array.filter(item => typeof item === "number")
+}
+
+
+console.log(filtrarNumeros(["ABBA", 1, "Ç", 2, 3, "Sete"]))
